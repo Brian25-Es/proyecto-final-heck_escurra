041d526c2415b9df643a1f73c088e75156ff349d
Brian25-Es <escurrabrian552@gmail.com>
2025-11-05 18:26:02 -0300

    cambios

--- login.php
+++ login.php
@@ -14,7 +14,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $result = $stmt->get_result();
     $user = $result->fetch_assoc();
 
-    if ($user && password_verify($password, $user["password"])) {
+    if ($user && $password($password, $user["password"])) {
 
         $_SESSION["usuario_id"]     = $user["id"];
         $_SESSION["usuario_nombre"] = $user["user"];
