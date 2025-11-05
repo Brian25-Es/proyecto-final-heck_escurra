2437f51c9fbdae22ebba0b568e2ba34a96af9938
Brian25-Es <escurrabrian552@gmail.com>
2025-11-05 16:57:05 -0300

    conexionBD

---
+++ configdatabase.php
@@ -0,0 +1,23 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $db_name = "biblioteca";  // debe coincidir con tu BD
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+    public function getConnection() {
+        try {
+            $this->conn = new PDO(
+                "mysql:host={$this->host};dbname={$this->db_name};charset=utf8",
+                $this->username,
+                $this->password
+            );
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            die("Error de conexión: " . $e->getMessage());
+        }
+        return $this->conn;
+    }
+}
+?>
