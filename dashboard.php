6914cff05860a7a8e3e9903b55af0973a277f752
FerJH536 <fer536210@gmail.com>
2025-11-18 21:45:59 -0300

    comentario de codigo

--- dashboard.php
+++ dashboard.php
@@ -193,9 +193,9 @@ function openTab(tabName) {
     event.target.classList.add("active");
 }
 
-/* =============================== */
-/* ALQUILAR LIBRO (YA TENÍAS ESTO) */
-/* =============================== */
+/* ============== */
+/* ALQUILAR LIBRO */
+/* ============== */
 function alquilarLibro(idLibro) {
     let usuarioSelect = document.getElementById("usuarioSelect-" + idLibro);
     let usuario_id = usuarioSelect.value;
